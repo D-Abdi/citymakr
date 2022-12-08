58aa4c681b02f7219276cfb9262feaea178bbb2f
Daniel <danielabdi12@gmail.com>
2022-12-08 16:32:58 +0100

    Add rotation to selected radial

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
 console.log("Loaded");
 
+let elementCount = 0;
+let selected = null;
+
 $(document).ready(function () {
   $(this).on("mousedown", function (e) {
     if (e.target.classList && e.target.classList.length === 0) {
-      var x = e.pageX;
-      var y = e.pageY;
+      const x = e.pageX;
+      const y = e.pageY;
+      const idString = `${x.toString() + y.toString()}`
 
       $("<div></div>")
         .css({ top: y, left: x })
         .addClass("base")
-        .attr("id", x.toString() + y.toString())
+        .attr("id", `${idString}`)
         .append(
           $("<div></div>")
             .addClass("sub1")
-            .animate({ opacity: "1", top: "-150%", left: "-175%" }, "fast"),
+            .animate({ opacity: "1", top: "200%", left: "-200%" }, "fast"),
         )
         .append(
           $("<div></div>")
             .addClass("sub2")
-            .animate({ opacity: "1", top: "-200%" }, "fast"),
+            .animate({ opacity: "1", top: "250%" }, "fast"),
         )
         .append(
           $("<div></div>")
             .addClass("sub3")
-            .animate({ opacity: "1", top: "-150%", left: "175%" }, "fast"),
+            .animate({ opacity: "1", top: "200%", left: "200%" }, "fast"),
         )
         .append($("<div></div>").addClass("slider"))
         .appendTo("body");
@@ -38,11 +42,19 @@ $(document).ready(function () {
         startAngle: "0",
         startAngle: "270",
         drag: function(event) {
-          $("#" + x + y)
+          $(`#${x.toString() + y.toString()}`)
             .css('transform', '')
             .css('transform', `rotate(${event.value}deg)`);
         }
       });
+      elementCount++
+    } else {
+      let idPath = e.originalEvent.path;
+      let foundId = idPath.find(x => x.id).id;
+      if (foundId) {
+        selected = foundId;
+        console.log(selected);
+      }
     }
   });
   $(this).on("click", function (e) {
